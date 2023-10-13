35b321cb561ee0a75cbe57e1c704c9cc62802cd0
DWalston <98560698+DWalston@users.noreply.github.com>
2023-10-13 19:51:16 -0400

    Create databaseManager.java

---
+++ Backend/databaseManager.java
@@ -0,0 +1,10 @@
+package com.vinayak.jdbc;
+ 
+import java.sql.*;
+
+public class databaseManager {
+  public class addItem (String tableName, String[] values) {
+  }
+  public class removeItem (String tableName, String[][] constraints) {
+  }
+}
