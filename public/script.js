7cfa36dacf2307cb2ed63cd6a6c2b0eaf64d8931
M Dattis <38086897+MssTake@users.noreply.github.com>
2023-10-30 14:50:03 -0400

    my brain hurts

--- public/script.js
+++ public/script.js
@@ -1,11 +1,3 @@
-// cannot remember what this was for
-function search(str) {
-   if (str.length == 0) {
-      document.getElementById("searchBar").innerHTML = "";
-      return;
-   }
-}
-
 function liveSearch() {
    var input = document.getElementById("searchBar");
    var filter = input.ariaValueMax.toUpperCase();
@@ -21,6 +13,83 @@ function liveSearch() {
          else {
             tr[length].style.display = "none"
          }
+      }
+   }
+}
+
+function clickToHide(hideThis) {
+   if (hideThis.classList.contains(showForNow) && !classList.toggle("hide")) {
+      switch (hideThis) {
+         case "name":
+            document.getElementById("itemName").classList.toggle("hide");
+            break;
+         case "type":
+            document.getElementById("itemType").classList.toggle("hide");
+            break;
+         case "series":
+            document.getElementById("itemSeries").classList.toggle("hide");
+            break;
+         case "shelf":
+            document.getElementById("itemShelf").classList.toggle("hide");
+            break;
+         case "section":
+            document.getElementById("itemSection").classList.toggle("hide");
+            break;
+         case "status":
+            document.getElementById("itemStatus").classList.toggle("hide");
+            break;
+         case "rating":
+            document.getElementById("itemRating").classList.toggle("hide");
+            break;
+         case "club":
+            document.getElementById("itemClub").classList.toggle("hide");
+            break;
+         case "description":
+            document.getElementById("itemDescription").classList.toggle("hide");
+            break;
+         case "other":
+            document.getElementById("itemOther").classList.toggle("hide");
+            break;
+         case "ID":
+            document.getElementById("itemID").classList.toggle("hide");
+            break;
+      }
+   }
+   else if (hideThis.classList.contains(showForNow) && classList.toggle("hide")) {
+      switch (hideThis) {
+         case "name":
+            document.getElementById("itemName").classList.toggle("show");
+            break;
+         case "type":
+            document.getElementById("itemType").classList.toggle("show");
+            break;
+         case "series":
+            document.getElementById("itemSeries").classList.toggle("show");
+            break;
+         case "shelf":
+            document.getElementById("itemShelf").classList.toggle("show");
+            break;
+         case "section":
+            document.getElementById("itemSection").classList.toggle("show");
+            break;
+         case "status":
+            document.getElementById("itemStatus").classList.toggle("show");
+            break;
+         case "rating":
+            document.getElementById("itemRating").classList.toggle("show");
+            break;
+         case "club":
+            document.getElementById("itemClub").classList.toggle("show");
+            break;
+         case "description":
+            document.getElementById("itemDescription").classList.toggle("show");
+            break;
+         case "other":
+            document.getElementById("itemOther").classList.toggle("show");
+            break;
+         case "ID":
+            document.getElementById("itemID").classList.toggle("show");
+            break;
       }
    }
 }
