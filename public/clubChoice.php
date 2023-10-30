9df1e0e5d40b70d50a9194e5ab44a37ff456aa9c
M Dattis <38086897+MssTake@users.noreply.github.com>
2023-10-30 11:23:22 -0400

    i am the biggest dingus in the group oh my god

--- public/clubChoice.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_register('club');
-$_SESSION['club']=$_POST['clubChoice'];
-session_destroy();
-?>
--- public/externalSearch.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_register('searchBar');
-$_SESSION['searchBar']=$_POST['query'];
-session_destroy();
-?>
