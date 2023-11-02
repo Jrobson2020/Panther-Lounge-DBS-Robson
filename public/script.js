b73881872929884e64e22498943e8365d1c3eebd
M Dattis <38086897+MssTake@users.noreply.github.com>
2023-11-02 15:34:55 -0400

    catalog boxes work

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 // catalog
 
+// live search in searchbar
 function liveSearch() {
    var input = document.getElementById("searchBar");
    var filter = input.ariaValueMax.toUpperCase();
@@ -19,80 +20,15 @@ function liveSearch() {
    }
 }
 
-function clickToHide(hideThis) {
-   if (hideThis.classList.contains(showForNow) && !classList.toggle("hide")) {
-      switch (hideThis) {
-         case "name":
-            document.getElementById("itemName").classList.toggle("hide");
-            break;
-         case "type":
-            document.getElementById("itemType").classList.toggle("hide");
-            break;
-         case "series":
-            document.getElementById("itemSeries").classList.toggle("hide");
-            break;
-         case "shelf":
-            document.getElementById("itemShelf").classList.toggle("hide");
-            break;
-         case "section":
-            document.getElementById("itemSection").classList.toggle("hide");
-            break;
-         case "status":
-            document.getElementById("itemStatus").classList.toggle("hide");
-            break;
-         case "rating":
-            document.getElementById("itemRating").classList.toggle("hide");
-            break;
-         case "club":
-            document.getElementById("itemClub").classList.toggle("hide");
-            break;
-         case "description":
-            document.getElementById("itemDescription").classList.toggle("hide");
-            break;
-         case "other":
-            document.getElementById("itemOther").classList.toggle("hide");
-            break;
-         case "ID":
-            document.getElementById("itemID").classList.toggle("hide");
-            break;
-      }
+// checkboxes will show/hide columns
+function clickToShow(targetColumn) {
+   if (document.getElementById(targetColumn).style.visibility === "hidden") {
+document.getElementById(targetColumn).style.visibility = "visible"
    }
-   else if (hideThis.classList.contains(showForNow) && classList.toggle("hide")) {
-      switch (hideThis) {
-         case "name":
-            document.getElementById("itemName").classList.toggle("show");
-            break;
-         case "type":
-            document.getElementById("itemType").classList.toggle("show");
-            break;
-         case "series":
-            document.getElementById("itemSeries").classList.toggle("show");
-            break;
-         case "shelf":
-            document.getElementById("itemShelf").classList.toggle("show");
-            break;
-         case "section":
-            document.getElementById("itemSection").classList.toggle("show");
-            break;
-         case "status":
-            document.getElementById("itemStatus").classList.toggle("show");
-            break;
-         case "rating":
-            document.getElementById("itemRating").classList.toggle("show");
-            break;
-         case "club":
-            document.getElementById("itemClub").classList.toggle("show");
-            break;
-         case "description":
-            document.getElementById("itemDescription").classList.toggle("show");
-            break;
-         case "other":
-            document.getElementById("itemOther").classList.toggle("show");
-            break;
-         case "ID":
-            document.getElementById("itemID").classList.toggle("show");
-            break;
-      }
+   else {
+
+         document.getElementById(targetColumn).style.visibility = "hidden"
+      document.getElementById(targetColumn).display = "none";
    }
 }
 
