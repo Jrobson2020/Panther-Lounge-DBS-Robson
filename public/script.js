a79a558716da99b01ddb8d1da5dcf6eb3f091290
M Dattis <38086897+MssTake@users.noreply.github.com>
2023-11-02 21:14:18 -0400

    fixing search in catalog

--- public/script.js
+++ public/script.js
@@ -1,14 +1,14 @@
 // live search in searchbar
 function liveSearch() {
    var input = document.getElementById("searchBar");
-   var filter = input.ariaValueMax.toUpperCase();
+   var searchUpCase = input.ariaValueMax.toUpperCase();
    var table = document.getElementById("catalog");
    var tr = table.getElementsByTagName("tr");
    for (var length = 0; length < tr.length; length++) {
       var td = tr[length].getElementsByTagName("td")[0];
       if (td) {
-         txtValue = td.textContent || td.innerText;
-         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+         var txtValue = td.textContent || td.innerText;
+         if (txtValue.toUpperCase().indexOf(searchUpCase) > -1) {
             tr[length].style.display = "";
          }
          else {
@@ -20,14 +20,19 @@ function liveSearch() {
 
 // show/hide content
 function clickToHide(targetColumn) {
-   if (document.getElementById(targetColumn).style.visibility === "visible") {
-      document.getElementById(targetColumn).style.visibility = "hidden"
-      document.getElementById(targetColumn).display = "none"
+   if (document.getElementsById(targetColumn).style.visibility === "visible") {
+      document.getElementsById(targetColumn).style.visibility = "hidden"
+      document.getElementsById(targetColumn).display = "none"
    }
    else {
-      document.getElementById(targetColumn).style.visibility = "visible";
-      document.getElementById(targetColumn).display = "block"
+      document.getElementsById(targetColumn).style.visibility = "visible";
+      document.getElementsById(targetColumn).display = "block"
    }
+}
+
+// copy id number
+function copyThis(number) {
+   navigator.clipboard.writeText(number);
 }
 
 // woahhh what's this? automatic date machine? so cool
@@ -35,9 +40,4 @@ function twoWeeks(coDate, ciDate) {
    plusTwoWeeks = addDays(document.getElementById(coDate), 14);
    ciDate = plusTwoWeeks;
    return ciDate;
-}
-
-// copy id number
-function copyThis(number) {
-   navigator.clipboard.writeText(number);
 }
